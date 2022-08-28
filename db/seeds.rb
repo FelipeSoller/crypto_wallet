33b4931af3a1c5809008ee6ef4caf3731033f520
Felipe Soller <felipe.souza@imaginedone.com.br>
2022-08-28 15:28:33 -0300

    add seed to create coins in data base

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'CADASTRANDO MOEDAS'
+
+Coin.create!(
+  description: 'Bitcoin',
+  acronym: 'BTC',
+  url_image: 'https://cdn.pixabay.com/photo/2018/04/02/14/48/bitcoin-logo-3284066_960_720.png'
+)
+
+Coin.create!(
+  description: 'Ethereum',
+  acronym: 'ETH',
+  url_image: 'https://spng.subpng.com/20180330/gkq/kisspng-ethereum-bitcoin-cryptocurrency-logo-tether-bitcoin-5abdfe01865749.6048300215224007695503.jpg'
+)
+
+Coin.create!(
+  description: 'Dash',
+  acronym: 'DASH',
+  url_image: 'https://seeklogo.com/images/D/dash-logo-4A14989CF5-seeklogo.com.png'
+)
+
+Coin.create!(
+  description: 'Dogecoin',
+  acronym: 'DOG',
+  url_image: 'https://upload.wikimedia.org/wikipedia/pt/d/d0/Dogecoin_Logo.png'
+)
+
+puts 'MOEDAS CADASTRADAS COM SUCESSO!'
